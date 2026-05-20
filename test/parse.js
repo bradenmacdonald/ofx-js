6644c6459744c9030beee0216c08e41669a2156c
Braden MacDonald <mail@bradenm.com>
2026-05-20 14:04:38 -0700

    test: expand tests - add more assertions

--- test/parse.js
+++ test/parse.js
@@ -1,17 +1,73 @@
 import { readFileSync } from 'fs';
 import { test } from 'node:test';
 import assert from 'node:assert/strict';
-import { parse } from '../ofx.js';
+import { parseSync as parse } from '../ofx.js';
 
 test('parse example1', async () => {
     const file = readFileSync(new URL('data/example1.ofx', import.meta.url), 'utf8');
     const data = await parse(file);
 
+    // Check header:
     assert.equal(data.header.OFXHEADER, '100');
     assert.equal(data.header.ENCODING, 'USASCII');
+    assert.equal(data.header.DATA, 'OFXSGML');
+    assert.equal(data.header.VERSION, '102');
+    assert.equal(data.header.SECURITY, 'NONE');
+    assert.equal(data.header.CHARSET, '1252');
+    assert.equal(data.header.COMPRESSION, 'NONE');
+    assert.equal(data.header.OLDFILEUID, 'NONE');
+    assert.equal(data.header.NEWFILEUID, 'NONE');
+    
+    // Check account info:
+    const currency = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.CURDEF;
+    assert.equal(currency, 'NZD');
+    const accountInfo = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKACCTFROM;
+    assert.deepEqual(accountInfo, {
+        ACCTID: '1234567-00',
+        ACCTTYPE: 'SAVINGS',
+        BANKID: '01',
+        BRANCHID: '0123',
+    });
 
+    // Check transaction list dates:
+    const startDate = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTSTART;
+    assert.equal(startDate, '20120101');
+    const endDate = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTEND;
+    assert.equal(endDate, '20121003');
+
+    // Check ledger balance:
+    const ledgerBalance = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL;
+    assert.deepEqual(ledgerBalance, {
+        BALAMT: '4225.79',
+        DTASOF: '20121004',
+    });
+
+    // Check transactions:
     const transactions = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN;
     assert.equal(transactions.length, 5);
+    assert.deepEqual(transactions[0], {
+        DTPOSTED: '20120928',
+        FITID: '20120928.0',
+        MEMO: 'Credit Interest Paid',
+        TRNAMT: '19.90', // Note: everything is a string, even amounts.
+        TRNTYPE: 'INT',
+    });
+    assert.deepEqual(transactions[1], {
+        DTPOSTED: '20120928',
+        FITID: '20120928.1',
+        MEMO: 'Withholding Tax',
+        TRNAMT: '-1.01',
+        TRNTYPE: 'DEBIT'
+    });
+    // ...
+    assert.deepEqual(transactions[4], {
+        DTPOSTED: '20120104',
+        FITID: '20120104.6',
+        MEMO: 'Debit Transfer Party',
+        NAME: 'Transfer',
+        TRNAMT: '-20.01',
+        TRNTYPE: 'DEBIT'
+    });
 
     const status = data.OFX.SIGNONMSGSRSV1.SONRS.STATUS;
     assert.equal(status.CODE, '0');
@@ -27,7 +83,15 @@ test('parse example2', async () => {
 
     const transactions = data.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN;
     assert.equal(transactions.length, 29);
-    assert.equal(transactions[0].TRNAMT, "-45.54");
+    assert.deepEqual(transactions[0], {
+        CHECKNUM: '*****',
+        DTPOSTED: '****',
+        FITID: '******',
+        MEMO: '*******',
+        NAME: 'FRAIS DE TUTELLE',
+        TRNAMT: '-45.54',
+        TRNTYPE: 'DEBIT',
+    });
 
     const status = data.OFX.SIGNONMSGSRSV1.SONRS.STATUS;
     assert.equal(status.CODE, '0');
